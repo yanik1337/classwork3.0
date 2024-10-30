2d223cf77575e63e7454786a17919874f322c083
yanik1337 <144943233+yanik1337@users.noreply.github.com>
2024-10-30 18:36:29 +0300

    objectextenseclass

---
+++ ObjectExtenseClass.cs
@@ -0,0 +1,19 @@
+﻿namespace ClassWork5.ObjectExtenseClass
+{
+    public class ObjectExtense
+    {
+        public static bool AreEqual<T>(T item1, T item2)
+        {
+            if  (item1 == null && item2 == null)
+            {
+                return true;
+            }
+
+            if (item1 == null || item2 == null)
+            {
+                return true;
+            }
+            return item1.Equals(item2);
+        }
+    }
+}
