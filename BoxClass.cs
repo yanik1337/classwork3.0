6baa1d019dc41daf3aa640d5e02220a4aa22e374
yanik1337 <144943233+yanik1337@users.noreply.github.com>
2024-10-30 18:35:23 +0300

    boxclass

---
+++ BoxClass.cs
@@ -0,0 +1,17 @@
+﻿namespace ClassWork5.BoxClass
+{
+    public class Box<T>
+    {
+        private T _item;
+
+        public void SetItem(T item)
+        {
+            _item = item;
+        }
+
+        public T GetItem() 
+        {
+            return _item;
+        }
+    }
+}
