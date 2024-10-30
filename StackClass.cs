a09031802914cf387a597e9d50e142edd7ef66af
yanik1337 <144943233+yanik1337@users.noreply.github.com>
2024-10-30 18:36:09 +0300

    3 zadanie

---
+++ StackClass.cs
@@ -0,0 +1,23 @@
+﻿namespace ClassWork5.StackClass
+{
+    public class Stack<T>
+    {
+        private List<T> _items = new List<T>();
+
+        public void Push(T item)
+        { 
+            _items.Add(item);
+        }
+
+        public T Pop()
+        {
+            if (_items.Count == 0)
+            {
+                throw new InvalidOperationException("пустой стек(");
+            }
+            T lastItem = _items[_items.Count - 1];
+            _items.RemoveAt(_items.Count - 1);
+            return lastItem;
+        }
+    }
+}
